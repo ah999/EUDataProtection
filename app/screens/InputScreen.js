8334bacd5a1067df655e7210eb3778906113d69d
Abrar Mohi Shafee <abrarmohi@yahoo.com>
2020-11-17 00:02:31 +0100

    database populated

--- app/screens/InputScreen.js
+++ app/screens/InputScreen.js
@@ -16,8 +16,34 @@ class InputScreen extends React.Component {
           onValueChange={(value) =>
             this.setState({country: value})
         }>
-          <Picker.Item label="Germany" value="germany" />
+          <Picker.Item label="Austria" value="austria" />
+          <Picker.Item label="Belgium" value="belgium" />
+          <Picker.Item label="Bulgaria" value="bulgaria" />
+          <Picker.Item label="Croatia" value="croatia" />
+          <Picker.Item label="Cyprus" value="cyprus" />
+          <Picker.Item label="Czech Republic" value="czechRepublic" />
           <Picker.Item label="Denmark" value="denmark" />
+          <Picker.Item label="Estonia" value="estonia" />
+          <Picker.Item label="Finland" value="finland" />
+          <Picker.Item label="France" value="france" />
+          <Picker.Item label="Germany" value="germany" />
+          <Picker.Item label="Greece" value="greece" />
+          <Picker.Item label="Hungary" value="hungary" />
+          <Picker.Item label="Ireland" value="ireland" />
+          <Picker.Item label="Italy" value="italy" />
+          <Picker.Item label="Latvia" value="latvia" />
+          <Picker.Item label="Lithuania" value="lithuania" />
+          <Picker.Item label="Luxembourg" value="luxembourg" />
+          <Picker.Item label="Malta" value="malta" />
+          <Picker.Item label="Netherlands" value="netherlands" />
+          <Picker.Item label="Norway" value="norway" />
+          <Picker.Item label="Poland" value="poland" />
+          <Picker.Item label="Portugal" value="portugal" />
+          <Picker.Item label="Romania" value="romania" />
+          <Picker.Item label="Slovakia" value="slovakia" />
+          <Picker.Item label="Slovenia" value="slovenia" />
+          <Picker.Item label="Spain" value="spain" />
+          <Picker.Item label="Sweden" value="sweden" />
         </Picker>
         
         <Button style={styles.button} title="Submit" onPress={() => this.props.navigation.navigate('Result', { country: this.state.country })} />
